5dea5daa715c93965b0f03a44ce3dd84f91f81b5
Lukas Wöhrl <lukas@woehrl.net>
2019-02-10 20:52:45 +0100

    reset to inital page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './webmontag-kassel-logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        Hallo Welt
+        { /*
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Hallo Webmontag Kassel
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        */}
       </div>
     );
   }
