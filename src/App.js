ccc3b71cf5dae5c0ede0681b269f76001c0eb840
Lukas Wöhrl <lukas@woehrl.net>
2019-02-11 19:26:01 +0100

    awesome edit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-//import logo from './webmontag-kassel-logo.png';
+import logo from './webmontag-kassel-logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hallo Welt
-        { /*
+        
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Hallo Webmontag Kassel
           </p>
         </header>
-        */}
+        
       </div>
     );
   }
